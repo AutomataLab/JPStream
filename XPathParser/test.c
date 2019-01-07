c91aea84bba1a192f79d910da9809b0b64e73536
sunxfancy <sunxfancy@gmail.com>
2019-01-07 14:59:02 -0800

    update

--- XPathParser/test.c
+++ XPathParser/test.c
@@ -17,5 +17,11 @@ XPathNode* Analysis(const char* data) {
 int main() {
     XPathNode* root = Analysis("$.store.book[?(@.price < 10)].title");
     xpn_PrintJSON(root);
+
+    root = Analysis("$[?(@.a)]");
+    xpn_PrintJSON(root);
+
+    root = Analysis("$[?(@.a&&(@.b||@.c))]");
+    xpn_PrintJSON(root);
     return 0;
 }
