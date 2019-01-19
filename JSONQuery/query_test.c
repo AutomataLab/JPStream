baa58e55cab18ce7e55c05eaa922377497c09a92
ljian006 <ljian006@ucr.edu>
2019-01-18 18:10:00 -0800

    add test cases

--- JSONQuery/query_test.c
+++ JSONQuery/query_test.c
@@ -15,9 +15,12 @@ void Test0()
 void Test1()
 {
     initialization();
-    file_name = "twitter_store1.txt"; 
-    jsonPath = "$.root[*].quoted_status.*.media[?(@.url)&&(@.media_url)].sizes.large.w";
-    jsonPath = "$.root[*].entities.hashtags[*].text"; 
+    file_name = "../dataset/twitter.json";
+    //file_name = "twitter_store1.txt"; 
+    //jsonPath = "$.root[*].quoted_status.*.media[*].sizes.large.w";
+    jsonPath = "$.root[?(@.id)].quoted_status.entities.user_mentions[?(@.indices)&&(@.id_str)].id";
+   //jsonPath = "$.root[*].quoted_status.*.media[?(@.url)&&(@.media_url)].sizes.large.w";
+    //jsonPath = "$.root[*].entities.hashtags[*].text"; 
     num_threads = 16;
     pversion = 0;
     warmup_flag = 1;
@@ -37,16 +40,18 @@ void Test1()
         printf("query execution exception, please check your input\n");
         return;
     }
-    writing_results();
+    writing_results(); printf("output %d\n", outputs[2].top_output);
     if(debug_flag==1) print_debug_info(); 
 }
 
 void Test2()
 {
     initialization();
-    file_name = "twitter_store1.txt";
-    jsonPath = "$.root[?(@.id)&&(@.user.screen_name)].quoted_status.entities.user_mentions[?(@.indices)&&(@.id_str)].id";
-    num_threads = 64;
+    file_name = "../dataset/twitter.json";
+    //file_name = "twitter_store1.txt";
+    jsonPath = "$.root[?(@.text)&&(@.contributors)].id"; 
+    //jsonPath = "$.root[?(@.id)&&(@.user.screen_name)].quoted_status.entities.user_mentions[?(@.indices)&&(@.id_str)].id";
+    num_threads = 16;
     pversion = 1;
     warmup_flag = 1;
     debug_flag = 1;
@@ -73,9 +78,10 @@ void Test2()
 void Test3()
 {
     initialization();
-    file_name = "bb.json";
+    file_name = "../dataset/bb.json";
     jsonPath = "$.root.products[?(@.sku)&&(@.productId)].categoryPath[?(@.name)].id";
-    num_threads = 1;
+    //jsonPath = "$.root.products[?(@.sku)&&(@.productId)].categoryPath[?(@.name)].id";
+    num_threads = 64;
     pversion = 1;
     warmup_flag = 1;
     debug_flag = 1;
@@ -101,9 +107,123 @@ void Test3()
 void Test4()
 {
     initialization();
-    file_name = "rows_uk.json";
-    jsonPath = "$.root.meta.view.columns[?(@.id)&&(@.name)&&(@.cachedContents)].position";
+    file_name = "../dataset/bb.json";
+    //file_name = "rows_uk.json";
+    jsonPath = "$.root.products[*].productId";
+    //jsonPath = "$.root.meta.view.columns[?(@.id)&&(@.name)&&(@.cachedContents)].position";
     num_threads = 16;
+    pversion = 0;
+    warmup_flag = 1;
+    debug_flag = 1;
+    percentage = 1;
+    xmlPath=(char*)malloc(2000*sizeof(char));
+    int ret = convertJSONPath(jsonPath, xmlPath);
+    if(ret == 0)
+    {
+        printf("Input JSONPath query in config is not correct, please open the file and check it again!\n");
+        return;
+    }
+    automata();
+    ret = execute_query();
+    if(ret==0)
+    {
+        printf("query execution exception, please check your input\n");
+        return;
+    }
+    writing_results();
+    if(debug_flag==1) print_debug_info();
+}
+
+void Test5()
+{
+    initialization();
+    file_name = "../dataset/rowstest.json";
+    jsonPath = "$.meta.view.columns[?(@.id)&&(@.name)&&(@.cachedContents)].position";
+    num_threads = 16;
+    pversion = 0;
+    warmup_flag = 1;
+    debug_flag = 1;
+    percentage = 1;
+    xmlPath=(char*)malloc(2000*sizeof(char));
+    int ret = convertJSONPath(jsonPath, xmlPath);
+    if(ret == 0)
+    {
+        printf("Input JSONPath query in config is not correct, please open the file and check it again!\n");
+        return;
+    }
+    automata();
+    ret = execute_query();
+    if(ret==0)
+    {
+        printf("query execution exception, please check your input\n");
+        return;
+    }
+    writing_results();
+    if(debug_flag==1) print_debug_info();
+}
+
+void Test6()
+{
+    initialization();
+    file_name = "../dataset/wiki.json";
+    jsonPath = "$.root[*].title";
+    num_threads = 16;
+    pversion = 1;
+    warmup_flag = 1;
+    debug_flag = 1;
+    percentage = 1;
+    xmlPath=(char*)malloc(2000*sizeof(char));
+    int ret = convertJSONPath(jsonPath, xmlPath);
+    if(ret == 0)
+    {
+        printf("Input JSONPath query in config is not correct, please open the file and check it again!\n");
+        return;
+    }
+    automata();
+    ret = execute_query();
+    if(ret==0)
+    {
+        printf("query execution exception, please check your input\n");
+        return;
+    }
+    writing_results();
+    if(debug_flag==1) print_debug_info();
+}
+
+void Test7()
+{
+    initialization();
+    file_name = "../dataset/wiki.json";
+    jsonPath = "$.root[*].claims.P150[?(@.id&&@.type)].mainsnak.property";
+    num_threads = 64;
+    pversion = 1;
+    warmup_flag = 1;
+    debug_flag = 1;
+    percentage = 10;
+    xmlPath=(char*)malloc(2000*sizeof(char));
+    int ret = convertJSONPath(jsonPath, xmlPath);
+    if(ret == 0)
+    {
+        printf("Input JSONPath query in config is not correct, please open the file and check it again!\n");
+        return;
+    }
+    automata();
+    ret = execute_query();
+    if(ret==0)
+    {
+        printf("query execution exception, please check your input\n");
+        return;
+    }
+    writing_results();
+    if(debug_flag==1) print_debug_info();
+}
+
+void Test8()
+{
+    initialization();
+    file_name = "../dataset/random.json";
+    jsonPath = "$.root[?(@.index&&@.guid)].friends[?(@.name)].id";
+    num_threads = 64;
     pversion = 0;
     warmup_flag = 1;
     debug_flag = 1;
@@ -129,10 +249,14 @@ void Test4()
 int main()
 {
     Test0();
-    /*Test1();
+    Test1();
     Test2();
     Test3();
-    Test4();*/
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
     return 0;
 }
 
